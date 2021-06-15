ba5ca50542fd1f3a6f670ee3a8638e39d3f5ecec
Philip Lindner <philip.lindner2@gmail.com>
2021-06-15 23:30:49 +0200

    command setup

--- src/main/java/at/livekit/plugin/Plugin.java
+++ src/main/java/at/livekit/plugin/Plugin.java
@@ -29,6 +29,7 @@ import at.livekit.api.map.PersonalPin;
 import at.livekit.authentication.AuthenticationHandler;
 import at.livekit.authentication.Pin;
 import at.livekit.authentication.Session;
+import at.livekit.commands.BaseCommands;
 import at.livekit.livekit.Economy;
 import at.livekit.livekit.Identity;
 import at.livekit.livekit.LiveKit;
@@ -167,6 +168,10 @@ public class Plugin extends JavaPlugin implements CommandExecutor, ILiveKitPlugi
 
 		//registers console listener (console enable check is done inside)
 		ConsoleListener.registerListener();
+
+		BaseCommands base = new BaseCommands();
+		getCommand("lk").setExecutor(base);
+		getCommand("lk").setTabCompleter(base);
     }
     
 	@EventHandler
