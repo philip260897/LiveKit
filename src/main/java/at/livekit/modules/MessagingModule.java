46300920682803f3866ba69f89817a7a5c86710c
Philip Lindner <philip260897@gmail.com>
2024-06-25 16:17:32 +0200

    messaging implementation

--- src/main/java/at/livekit/modules/MessagingModule.java
+++ src/main/java/at/livekit/modules/MessagingModule.java
@@ -16,7 +16,6 @@ import org.bukkit.OfflinePlayer;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-import at.livekit.api.chat.ChatMessage;
 import at.livekit.api.pm.IMessagingInterface;
 import at.livekit.api.pm.MessageGroup;
 import at.livekit.api.pm.MessagingAdapter;
@@ -58,13 +57,19 @@ public class MessagingModule extends BaseModule implements IMessagingInterface {
                         }
 
                         if(pm.getChannel().equals("default")) {
-                            Plugin.debug("Message 1");
                             if(LiveKit.getInstance().getConnectedClients(pm.getReceiver().toString()).size() > 0) {
-                                Plugin.debug("Message 2");
                                 synchronized(messageUpdates) {
                                     List<PrivateMessage> messages = messageUpdates.containsKey(pm.getReceiver().toString()) ? messageUpdates.get(pm.getReceiver().toString()) : new ArrayList<>();
                                     messages.add(pm);
                                     messageUpdates.put(pm.getReceiver().toString(), messages);
+                                }
+                            }
+
+                            if(!pm.getSender().equals(pm.getReceiver()) && LiveKit.getInstance().getConnectedClients(pm.getSender().toString()).size() > 0) {
+                                synchronized(messageUpdates) {
+                                    List<PrivateMessage> messages = messageUpdates.containsKey(pm.getSender().toString()) ? messageUpdates.get(pm.getSender().toString()) : new ArrayList<>();
+                                    messages.add(pm);
+                                    messageUpdates.put(pm.getSender().toString(), messages);
                                 }
                             }
                         } else if(pm.getChannel().equals("group")) {
@@ -100,53 +105,6 @@ public class MessagingModule extends BaseModule implements IMessagingInterface {
         }
     }
 
-    /*@Action(name = "listChats", sync = false) 
-    public IPacket listChatsAction(Identity identity, ActionPacket packet) {
-        Set<UUID> uuids = new HashSet<UUID>();
-        try {
-            uuids.addAll(Plugin.getStorage().load(PrivateMessage.class, "receiver", identity.getUuid().toString()).stream().map((c) -> c.getSender()).collect(Collectors.toSet()));
-            uuids.addAll(Plugin.getStorage().load(PrivateMessage.class, "sender", identity.getUuid().toString()).stream().map((c) -> c.getReceiver()).collect(Collectors.toSet()));
-
-        } catch(Exception e) { e.printStackTrace(); }
-
-        JSONArray array = new JSONArray();
-        for(UUID uuid : uuids) {
-            JSONObject json = new JSONObject();
-            json.put("uuid", uuid.toString());
-            array.put(json);
-        }
-    }*/
-
-    @Action(name = "SendPrivateMessage", sync = false)
-    public IPacket sendPrivateMessage(Identity identity, ActionPacket packet) {
-        JSONObject json = packet.getData();
-        UUID sender = UUID.fromString(identity.getUuid());
-        UUID receiver = UUID.fromString(json.getString("receiver"));
-
-        String message = json.getString("message");
-        String channel = json.getString("channel");
-
-        if(channel.equals("default")) {
-            PrivateMessage pm = PrivateMessage.create(sender, receiver, message);
-            try {
-                Plugin.getStorage().create(pm);
-            } catch(Exception e) { e.printStackTrace();}
-
-            Bukkit.getScheduler().callSyncMethod(Plugin.getInstance(), new Callable<Void>() {
-                @Override
-                public Void call() {
-                    OfflinePlayer senderPlayer = Bukkit.getOfflinePlayer(sender);
-                    OfflinePlayer receiverPlayer = Bukkit.getOfflinePlayer(receiver);
-                    
-                    onPrivateMessageReceived(senderPlayer, receiverPlayer, message);
-                    return null;
-                }
-            });
-        }
-
-        return new StatusPacket(1);
-    }
-
     @Override
     public void onGroupMessageReceived(OfflinePlayer sender, MessageGroup receiver, String message) {
         if(messagingAdapter != null) {
@@ -178,22 +136,129 @@ public class MessagingModule extends BaseModule implements IMessagingInterface {
     public IPacket onJoinAsync(Identity identity) {
         JSONObject json = new JSONObject();
         JSONArray messages = new JSONArray();
-        synchronized(messageUpdates) {
-            if(messageUpdates.containsKey(identity.getUuid())) {
-                for(PrivateMessage pm : messageUpdates.get(identity.getUuid())) {
-                    messages.put(pm.toJson());
-                }
-                messageUpdates.remove(identity.getUuid());
-            }
+        List<PrivateMessage> pms = getLastPrivateMessageForEachChat(UUID.fromString(identity.getUuid()));
+        for(PrivateMessage pm : pms) {
+            messages.put(pm.toJson());
         }
         json.put("messages", messages);
+        json.put("uuid", identity.getUuid());
         return new ModuleUpdatePacket(this, json, true);
     }
 
     @Override
     public Map<Identity, IPacket> onUpdateAsync(List<Identity> identities) {
         Map<Identity, IPacket> responses = new HashMap<Identity, IPacket>();
-        for(Identity identity : identities) responses.put(identity, onJoinAsync(identity));
+        for(Identity identity : identities) {
+
+            JSONObject json = new JSONObject();
+            JSONArray messages = new JSONArray();
+            synchronized(messageUpdates) {
+                if(messageUpdates.containsKey(identity.getUuid())) {
+                    for(PrivateMessage pm : messageUpdates.get(identity.getUuid())) {
+                        messages.put(pm.toJson());
+                    }
+                    messageUpdates.remove(identity.getUuid());
+                }
+            }
+            json.put("messages", messages);
+            responses.put(identity, new ModuleUpdatePacket(this, json, false));
+        }
         return responses;
+    }
+
+    @Action(name = "GetPrivateMessages", sync = false)
+    public IPacket actionGetPrivateMessages(Identity identity, ActionPacket packet) {
+        JSONObject json = packet.getData();
+        UUID sender = UUID.fromString(identity.getUuid());
+        UUID receiver = UUID.fromString(json.getString("receiver"));
+        long timestamp = json.getLong("timestamp");
+
+        List<PrivateMessage> messages = getPrivateMessages(sender, receiver).stream().filter((c) -> c.getTimestamp() > timestamp).collect(Collectors.toList());
+
+        JSONArray array = new JSONArray();
+        for(PrivateMessage pm : messages) {
+            array.put(pm.toJson());
+        }
+
+        JSONObject response = new JSONObject();
+        response.put("messages", array);
+        return new StatusPacket(1, response);
+    }
+
+    @Action(name = "ListChats", sync = false) 
+    public IPacket listChatsAction(Identity identity, ActionPacket packet) {
+        List<PrivateMessage> messages = getLastPrivateMessageForEachChat(UUID.fromString(identity.getUuid()));
+
+        JSONArray array = new JSONArray();
+        for(PrivateMessage pm : messages) {
+            JSONObject json = new JSONObject();
+            json.put("sender", pm.getSender().toString());
+            json.put("receiver", pm.getReceiver().toString());
+            json.put("message", pm.getMessage());
+            json.put("timestamp", pm.getTimestamp());
+            array.put(json);
+        }
+
+        JSONObject response = new JSONObject();
+        response.put("chats", array);
+
+        return new StatusPacket(1, response);
+    }
+
+    @Action(name = "SendPrivateMessage", sync = true)
+    public IPacket sendPrivateMessage(Identity identity, ActionPacket packet) {
+        JSONObject json = packet.getData();
+        UUID sender = UUID.fromString(identity.getUuid());
+        UUID receiver = UUID.fromString(json.getString("receiver"));
+
+        String message = json.getString("message");
+        String channel = json.getString("channel");
+
+        if(channel.equals("default")) {
+
+            
+            PrivateMessage pm = PrivateMessage.create(sender, receiver, message);
+            messageQueue.add(pm);
+
+
+
+            OfflinePlayer senderPlayer = Bukkit.getOfflinePlayer(sender);
+            OfflinePlayer receiverPlayer = Bukkit.getOfflinePlayer(receiver);
+                    
+            onPrivateMessageReceived(senderPlayer, receiverPlayer, message);
+            //return null;
+
+        }
+
+        return new StatusPacket(1);
+    }
+
+    private List<PrivateMessage> getPrivateMessages(UUID sender, UUID receiver) {
+        List<PrivateMessage> messages = new ArrayList<PrivateMessage>();
+        try {
+            messages.addAll(Plugin.getStorage().load(PrivateMessage.class, "sender", sender).stream().filter((c) -> c.getReceiver().equals(receiver)).collect(Collectors.toList()));
+            if(!sender.equals(receiver)) {
+                messages.addAll(Plugin.getStorage().load(PrivateMessage.class, "receiver", sender).stream().filter((c) -> c.getSender().equals(receiver)).collect(Collectors.toList()));
+            }
+        } catch(Exception e) { e.printStackTrace(); }
+        return messages;
+    }
+
+    private List<PrivateMessage> getLastPrivateMessageForEachChat(UUID sender) {
+        List<PrivateMessage> messages = new ArrayList<PrivateMessage>();
+        try {
+            Set<UUID> uuids = new HashSet<UUID>();
+            uuids.addAll(Plugin.getStorage().load(PrivateMessage.class, "receiver", sender).stream().map((c) -> c.getSender()).collect(Collectors.toSet()));
+            uuids.addAll(Plugin.getStorage().load(PrivateMessage.class, "sender", sender).stream().map((c) -> c.getReceiver()).collect(Collectors.toSet()));
+
+            for(UUID uuid : uuids) {
+                List<PrivateMessage> chatMessages = getPrivateMessages(sender, uuid);
+                chatMessages.sort((a, b) -> Long.compare(a.getTimestamp(), b.getTimestamp()));
+                if(chatMessages.size() > 0) {
+                    messages.add(chatMessages.get(chatMessages.size() - 1));
+                }
+            }
+        } catch(Exception e) { e.printStackTrace(); }
+        return messages;
     }
 }
