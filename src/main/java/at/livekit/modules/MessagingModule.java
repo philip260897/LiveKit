00ddcfd20d837cb72562ba3b943afd191df5c660
Philip Lindner <philip260897@gmail.com>
2024-06-25 23:29:51 +0200

    cleanup messaging module

--- src/main/java/at/livekit/modules/MessagingModule.java
+++ src/main/java/at/livekit/modules/MessagingModule.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
-import java.util.concurrent.Callable;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.stream.Collectors;
 
