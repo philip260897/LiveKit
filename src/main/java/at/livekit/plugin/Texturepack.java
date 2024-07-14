7091e9576344464691796fcadf438e48fef50908
Philip Lindner <philip260897@gmail.com>
2024-07-14 13:56:30 +0200

    cleanup

--- src/main/java/at/livekit/plugin/Texturepack.java
+++ src/main/java/at/livekit/plugin/Texturepack.java
@@ -5,7 +5,6 @@ import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
 
